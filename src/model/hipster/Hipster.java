8c1946e99474c9d84718418d5d6106203959ab69
tristangaebler <tgae6529@740s-pl-115.local>
2015-11-19 08:21:02 -0700

    Added the books array
    Added the description of books

--- src/model/hipster/Hipster.java
+++ src/model/hipster/Hipster.java
@@ -1,26 +1,52 @@
 package model.hipster;
 
+
 public class Hipster
 {
 	private String name;
 	private String [] hipsterPhrases;
-	//private Book [] hipsterBooks;
+	private Book [] hipsterBooks;
 	
 	public Hipster()
 	{
 		this.name = "";
 		this.hipsterPhrases = new String[4];
-		//this.hipsterBooks = new Books[3];
+		this.hipsterBooks = new Book[3];
 		setUpArray();
 		setUpBooks();
 	}
 	
+
 	private void setUpArray()
 	{
 		hipsterPhrases[0] = "Sorry, I only use products that are still in beta.";
 		hipsterPhrases[1] = "When I die, I'm leaving my body to art.";
 		hipsterPhrases[2] = "My music doesn’t really have a genre, but if I had to define it I’d probably say Organic Garage";
 		hipsterPhrases[3] = "They sound so much better on vinyl";
+	}
+	
+	private void setUpBooks()
+	{
+		Book firstBook, secondBook, thirdBook; //Declaring multiple variables of the same type on the same line 
+		firstBook = new Book();
+		firstBook.setAuthor("Micku Kaku");
+		firstBook.setTitle("Physics of the future");
+		firstBook.setSubject("Science");
+		firstBook.setPageCount(416);
+		firstBook.setPrice(19.68);
+		
+		secondBook = new Book();
+		secondBook.setAuthor("Walter Isaacson");
+		secondBook.setTitle("Steve Jobs");
+		secondBook.setSubject("Biography");
+		secondBook.setPageCount(655);
+		secondBook.setPrice(22.28);
+		
+		thirdBook = new Book(368, "Frank McCourt", "Angela's Ashes", "Memoir", 18.03);
+		
+		hipsterBooks[0] = firstBook;
+		hipsterBooks[1] = secondBook;
+		hipsterBooks[2] = thirdBook;		
 	}
 	
 	public String getName()
@@ -42,6 +68,14 @@ public class Hipster
 	{
 		this.hipsterPhrases = hipsterPhrases;
 	}
+	
+	public Book[] getHipsterBooks()
+	{
+		return hipsterBooks;
+	}
 
-	private void setUpBooks(){}
+	public void setHipsterBooks(Book[] hipsterBooks)
+	{
+		this.hipsterBooks = hipsterBooks;
+	}
 }
